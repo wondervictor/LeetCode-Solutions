2c9ee271ba0f4f8c1a099c688f0561dcf5021899
wondervictor <victorchanchina@gmail.com>
2017-07-09 13:56:56 +0800

    add 74

---
+++ 74-Search a 2D Matrix /main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func searchInVector(mat [][]int, target, index int) bool {
+	dim := len(mat[index])
+	left := 0
+	right := dim
+	for left < right {
+		mid := (left + right) / 2
+		if mat[index][mid] == target {
+			return true
+		} else if mat[index][mid] > target {
+			right = mid-1
+		} else if mat[index][mid] < target {
+			left = mid + 1
+		}
+	}
+	return false
+}
+
+func searchMatrix(matrix [][]int, target int) bool {
+	dimX := len(matrix)
+	if dimX == 0 {
+		return false
+	}
+	dimY := len(matrix[0])
+	if dimY == 0 {
+		return false
+	}
+	
+	up := 0
+	down := dimX
+	for up < down {
+		mid := (down + up)/2
+		if matrix[mid][0] == target {
+			return true
+		}
+		if matrix[mid][0] > target {
+			down = mid-1
+		} else if matrix[mid][0] < target {
+			if matrix[mid][dimY-1] == target {
+				return true
+			} else if matrix[mid][dimY-1] > target {
+				return searchInVector(matrix, target, mid)
+			} else {
+				up = mid + 1
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+
+}
