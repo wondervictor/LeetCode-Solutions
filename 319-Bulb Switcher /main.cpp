8acc97b5612d6e23be4fd5199365dcb443116de0
wondervictor <victorchanchina@gmail.com>
2018-12-11 10:11:21 +0800

    [AC]class Solution { public:     int bulbSwitch(int n) {         return (int)sqrt(n);     } };

---
+++ 319-Bulb Switcher /main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+      return (int)sqrt(n);
+    }
+};
+
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
